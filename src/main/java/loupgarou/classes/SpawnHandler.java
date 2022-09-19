8f7255883e1d5d935319608b10e3204348a1c45e
Switch <anthony_du_01@hotmail.fr>
2022-09-19 20:14:03 +0200

    move SpawnHandler

---
+++ src/main/java/loupgarou/classes/SpawnHandler.java
@@ -0,0 +1,44 @@
+package loupgarou.classes;
+
+import java.util.List;
+
+import org.bukkit.Location;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import loupgarou.App;
+import loupgarou.classes.utils.Utils;
+
+public class SpawnHandler {
+    public static void handleSpawn(CommandSender sender, String[] args, List<Location> locations, String action,
+            App mainClass) {
+        Integer spawnCount = locations.size();
+        if (args.length == 1) {
+            sender.sendMessage(String.format("Il y a actuellement %s lieu(x) de spawn dans la config.",
+                    spawnCount.toString()));
+        } else {
+            if (!Utils.isInteger(args[1])) {
+                sender.sendMessage("Tu dois saisir un chiffre chef");
+            }
+            Integer index = Integer.parseInt(args[1]) - 1;
+            if (index < 0 || index >= spawnCount) {
+                sender.sendMessage("Pas de spawn à cet index");
+            } else {
+                switch (action) {
+                    case "check":
+                        Location spawnToCheck = locations.get(index);
+                        Player playerToTeleport = (Player) sender;
+                        playerToTeleport.teleport(spawnToCheck);
+                        break;
+                    case "delete":
+                        locations.remove(locations.get(index));
+                        mainClass.saveConfig();
+                        mainClass.reloadConfig();
+                        sender.sendMessage("La position a bien été supprimée !");
+                        break;
+                }
+            }
+
+        }
+    }
+}
